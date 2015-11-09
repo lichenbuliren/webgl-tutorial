ca9aa2c51f77bbe69ef60dddf5a3f62a1ce33bf7
lichenbuliren <617106268@qq.com>
2015-11-09 21:33:19 +0800

    lession1

--- js/lession1.js
+++ js/lession1.js
@@ -1,44 +1,26 @@
 window.onload = function() {
-    var canvas = document.getElementById('webgl-demo'),
-        gl;
+    // 抓取作为Canvas容器的div标签
+    var demo = document.getElementById('webgl-demo');
 
-    function initWebGL(canvas) {
-        try {
-            gl = canvas.getContext('experimental-webgl');
-            console.log(gl);
-        } catch (e) {
-            var msg = 'Error creating WebGL Context!:' + e.toString();
-            alert(msg);
-            throw Error(msg);
-        }
-    }
+    // 创建 Three.js 渲染器，并添加到div标签中
+    var renderer = new THREE.WebGLRenderer();
+    renderer.setSize(demo.offsetWidth, demo.offsetHeight);
+    demo.appendChild(renderer.domElement);
 
-    function initViewport(gl, canvas) {
-        gl.viewport(0, 0, canvas.width, canvas.height);
+    // 创建 Three.js 场景
+    var scene = new THREE.Scene();
 
-    }
+    // 创建相机，并添加到场景中
+    var camera = new THREE.PerspectiveCamera(45, demo.offsetWidth / demo.offsetHeight, 1, 4000);
+    camera.position.set(0, 0, 3.3333);
+    scene.add(camera);
 
-    function createSquare(gl) {
-        var vertexBuffer;
-        vertexBuffer = gl.createBuffer();
-        gl.bindBuffer(gl.ARRAY_BUFFER, vertexBuffer);
-        var verts = [
-            .5, .5, 0.0, -.5, .5, 0.0,
-            .5, -.5, 0.0, -.5, -.5, 0.0
-        ];
+    // 创建一个矩形几何体，并添加到场景中
+    var geometry = new THREE.PlaneGeometry(1, 1);
+    var mesh = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial());
 
-        gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(verts), gl.STATIC_DRAW);
+    scene.add(mesh);
 
-        var square = {
-            buffer: vertexBuffer,
-            vertSize: 3,
-            nVerts: 4,
-            primtype: gl.TRIANGLE_STRIP
-        };
-
-        return square;
-    }
-
-    initWebGL(canvas);
-    initViewport(gl, canvas);
+    // 渲染绘制
+    renderer.render(scene, camera);
 }
